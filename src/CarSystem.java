aa8b990b4b2f3045b1eeeda70fe058fc2b880ac5
Daniel Engelstad <engelstad.daniel@gmail.com>
2022-02-16 11:03:35 +0100

    Added average

--- src/CarSystem.java
+++ src/CarSystem.java
@@ -1,11 +1,16 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class CarSystem {
 
     ArrayList<Person> personList = new ArrayList<>();
     ArrayList<Car> carList = new ArrayList<>();
+
+    ArrayList<Integer> personAgeList = new ArrayList<>();
+    ArrayList<Integer> carAgeList = new ArrayList<>();
+    ArrayList<Integer> carMileageList = new ArrayList<>();
+
+    Calendar calendar = Calendar.getInstance();
+    //int currentYear = calendar.get(Calendar.YEAR);
 
     static void printMenu() {
         String menu = "** CAR SYSTEM MENU **\n";
@@ -15,7 +20,8 @@ public class CarSystem {
         menu += "4 - Register new personal car\n";
         menu += "5 - Add test data\n";
         menu += "6 - Display all owners and their cars\n";
-        menu += "7 - Shut down system\n";
+        menu += "7 - Display average: person age, car age, car mileage\n";
+        menu += "8 - Shut down system\n";
 
         System.out.println(menu);
     }
@@ -23,7 +29,7 @@ public class CarSystem {
     public void menu() {
 
         int choice = 0;
-        while (choice != 7) {
+        while (choice != 8) {
             printMenu();
             choice = getChoice();
             switch (choice) {
@@ -46,6 +52,9 @@ public class CarSystem {
                     display();
                     break;
                 case 7:
+                    displayAverage();
+                    break;
+                case 8:
                     System.out.println("System is shutting down...");
                     break;
                 default:
@@ -60,7 +69,7 @@ public class CarSystem {
     int getChoice() {
         int i = getInteger();
 
-        return i < 8 ? i : 0;
+        return i < 9 ? i : 0;
     }
 
     private int getInteger() {
@@ -77,8 +86,9 @@ public class CarSystem {
         var newPerson = new Person();
         System.out.print("Name: ");
         newPerson.setName(scanner.nextLine());
-        System.out.println("Year of birth: ");
+        System.out.print("Year of birth: ");
         newPerson.setBirthYear(getInteger());
+        personAgeList.add((calendar.get(Calendar.YEAR) - newPerson.getBirthYear()));
         return newPerson;
     }
 
@@ -97,6 +107,8 @@ public class CarSystem {
         newTruck.setMileage(getInteger());
         System.out.print("Mileage date: ");
         newTruck.setMileageDate(scanner.nextLine());
+        carAgeList.add((calendar.get(Calendar.YEAR) - newTruck.getProductionYear()));
+        carMileageList.add(newTruck.getMileage());
         return newTruck;
     }
 
@@ -115,6 +127,8 @@ public class CarSystem {
         newVan.setMileage(getInteger());
         System.out.print("Mileage date: ");
         newVan.setMileageDate(scanner.nextLine());
+        carAgeList.add((calendar.get(Calendar.YEAR) - newVan.getProductionYear()));
+        carMileageList.add(newVan.getMileage());
         return newVan;
     }
 
@@ -133,7 +147,41 @@ public class CarSystem {
         newPersonal.setMileage(getInteger());
         System.out.print("Mileage date: ");
         newPersonal.setMileageDate(scanner.nextLine());
+        carAgeList.add((calendar.get(Calendar.YEAR) - newPersonal.getProductionYear()));
+        carMileageList.add(newPersonal.getMileage());
         return newPersonal;
+    }
+
+    public void createTestData() {
+        Person person1 = new Person ("Daniel Engelstad", 2000);
+        Person person2 = new Person ("Navn Generisk", 1943);
+        Person person3 = new Person ("Saniel Satanstad", 2003);
+        Person person4 = new Person ("Navnar Navnesen", 1982);
+        personList.addAll(List.of(new Person[]{person1, person2, person3, person4}));
+
+        personAgeList.addAll(List.of(
+                (calendar.get(Calendar.YEAR) - person1.birthYear),
+                (calendar.get(Calendar.YEAR) - person2.birthYear),
+                (calendar.get(Calendar.YEAR) - person1.birthYear),
+                (calendar.get(Calendar.YEAR) - person4.birthYear)));
+
+        Car car1 = new Truck("Volvo", "FM LNG",  2016, 42000, "2018-06-06", person1);
+        Car car2 = new Van("Ford", "Transit", 2021, 3020, "2022-02-15", person2);
+        Car car3 = new Personal("Toyota", "Prius", 2009, 162903, "2021-10-11", person3);
+        Car car4 = new Personal("Ferrari", "F150", 2019, 50006, "2020-03-01", person4);
+        carList.addAll(List.of(new Car[]{car1, car2, car3, car4}));
+
+        carAgeList.addAll(List.of(
+                (calendar.get(Calendar.YEAR) - car1.getProductionYear()),
+                (calendar.get(Calendar.YEAR) - car2.getProductionYear()),
+                (calendar.get(Calendar.YEAR) - car3.getProductionYear()),
+                (calendar.get(Calendar.YEAR) - car4.getProductionYear())));
+
+        carMileageList.addAll(List.of(
+                car1.getMileage(),
+                car2.getMileage(),
+                car3.getMileage(),
+                car4.getMileage()));
     }
 
     public void display() {
@@ -147,19 +195,26 @@ public class CarSystem {
         }
     }
 
-    public void createTestData() {
-        Person person1 = new Person ("Daniel Engelstad", 2000);
-        Person person2 = new Person ("Navn Generisk", 1943);
-        Person person3 = new Person ("Saniel Satanstad", 2003);
-        Person person4 = new Person ("Navnar Navnesen", 1982);
-        personList.addAll(List.of(new Person[]{person1, person2, person3, person4}));
+    public void displayAverage() {
+        OptionalDouble averagePersonAge = personAgeList
+                .stream()
+                .mapToDouble(a -> a)
+                .average();
+        System.out.println("Average person age: " + averagePersonAge.getAsDouble());
+        System.out.println();
 
-        Car car1 = new Truck("Volvo", "FM LNG",  2016, 42000, "2018-06-06", person1);
-        Car car2 = new Van("Ford", "Transit", 2021, 3020, "2022-02-15", person2);
-        Car car3 = new Personal("Toyota", "Prius", 2009, 162903, "2021-10-11", person3);
-        Car car4 = new Personal("Ferrari", "F150", 2019, 50006, "2020-03-01", person4);
-        carList.addAll(List.of(new Car[]{car1, car2, car3, car4}));
+        OptionalDouble averageCarAge = carAgeList
+                .stream()
+                .mapToDouble(a -> a)
+                .average();
+        System.out.println("Average car age: " + averageCarAge.getAsDouble());
+        System.out.println();
 
-
+        OptionalDouble averageCarMileage = carMileageList
+                .stream()
+                .mapToDouble(a -> a)
+                .average();
+        System.out.println("Average car mileage: " + averageCarMileage.getAsDouble());
+        System.out.println();
     }
 }
